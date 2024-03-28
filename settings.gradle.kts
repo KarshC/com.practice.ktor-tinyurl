c6b543c176db0ba3aa1d241be33a64d15ed88fa4
karshC <karshpsu@gmail.com>
2024-03-28 11:34:20 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.practice.ktor-tinyurl"
---
+++ src/main/kotlin/com/practice/Application.kt
@@ -0,0 +1,14 @@
+package com.practice
+
+import com.practice.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureRouting()
+}
