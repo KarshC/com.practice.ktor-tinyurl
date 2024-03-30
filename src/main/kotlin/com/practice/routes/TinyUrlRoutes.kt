39f44e967492e4a74e82d67b7dcd406f0a191a28
karshC <karshpsu@gmail.com>
2024-03-29 21:04:02 -0400

    updating the route

--- src/main/kotlin/com/practice/routes/TinyUrlRoutes.kt
+++ src/main/kotlin/com/practice/routes/TinyUrlRoutes.kt
@@ -8,45 +8,47 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.tinyUrlRoutes() {
-    post("/") {
-        val request = call.receive<UrlRequest>()
+    route("/tinyurlgenerator") {
+        post("/") {
+            val request = call.receive<UrlRequest>()
 
-        if (request.url.isNullOrEmpty()) {
-            call.respond(HttpStatusCode.BadRequest, "Missing field: url")
-            return@post
+            if (request.url.isNullOrEmpty()) {
+                call.respond(HttpStatusCode.BadRequest, "Missing field: url")
+                return@post
+            }
+
+            val existingShortenedUrl = urlMap.values.find { it.longUrl == request.url }
+            if (existingShortenedUrl != null) {
+                call.respond(existingShortenedUrl)
+                return@post
+            }
+
+            val key = generateKey(request.url)
+            val shortenedUrl = TinyUrlResponse(key, request.url, "http://localhost/$key")
+            urlMap[key] = shortenedUrl
+
+            call.respond(shortenedUrl)
         }
+        delete("/{key}") {
+            val key = call.parameters["key"]
+            val removedUrl = urlMap.remove(key)
 
-        val existingShortenedUrl = urlMap.values.find { it.longUrl == request.url }
-        if (existingShortenedUrl != null) {
-            call.respond(existingShortenedUrl)
-            return@post
+            if (removedUrl != null) {
+                call.respond(HttpStatusCode.OK)
+            } else {
+                call.respond(HttpStatusCode.NotFound)
+            }
         }
+        get("/{key}") {
+            val key = call.parameters["key"]
+            val shortenedUrl = urlMap[key]
 
-        val key = generateKey(request.url)
-        val shortenedUrl = TinyUrlResponse(key, request.url, "http://localhost/$key")
-        urlMap[key] = shortenedUrl
-
-        call.respond(shortenedUrl)
-    }
-    delete("/{key}") {
-        val key = call.parameters["key"]
-        val removedUrl = urlMap.remove(key)
-
-        if (removedUrl != null) {
-            call.respond(HttpStatusCode.OK)
-        } else {
-            call.respond(HttpStatusCode.NotFound)
-        }
-    }
-    get("/{key}") {
-        val key = call.parameters["key"]
-        val shortenedUrl = urlMap[key]
-
-        if (shortenedUrl != null) {
-            call.response.headers.append(HttpHeaders.Location, shortenedUrl.longUrl)
-            call.respond(HttpStatusCode.Found, "")
-        } else {
-            call.respond(HttpStatusCode.NotFound)
+            if (shortenedUrl != null) {
+                call.response.headers.append(HttpHeaders.Location, shortenedUrl.longUrl)
+                call.respond(HttpStatusCode.Found, "")
+            } else {
+                call.respond(HttpStatusCode.NotFound)
+            }
         }
     }
 
